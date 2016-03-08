518fd70d3e914a5f34ded892b4afd061e0df04ac
skip_xtian <rod@refactor.com>
2016-03-08 22:16:26 +1000

    POSTing to openweather

--- CaptivePortal-WIO.ino
+++ CaptivePortal-WIO.ino
@@ -1,24 +1,40 @@
+/*
+ * NOTES: 
+ * Sign up to openweathermap.org
+ * change your SSID,password
+ * enter the Weather stations name, Altitude & GPS coordinates
+ * 
+ * It will post data every 20 mins and include rainfall once an hour.
+ */
+
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
 
 #include <Adafruit_BMP085.h>
 #include <DHT.h>
 #include <Wire.h>  
 
-const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 0, 250);
-DNSServer dnsServer;
-ESP8266WebServer webServer(80);
 
-int updateTime = 60000;  //Once a minute
-long timeNow = -updateTime;
+#define server "openweathermap.org" //Do not change!
+
+//Enter you Wifi SSID and Password
+#define SSID ""
+#define password ""
+
+//Enter here the BASE64 encoded Credentials in the form <Username>:<Password>, www.base64encode.org   eg: encode bob:builder == Ym9iOmJ1aWxkZXI=
+#define CredBase64 "" 
+
+//retrieve your altitude & GPS coordinates from http://www.gps-coordinates.net/
+const String StationName = "Wio-01"; //Enter the Station Name how it will be displayed on Openweathermap.org
+const String lat = "-27.938179"; //Latitude goes here
+const String lng = "153.323263"; //Longitude goes here
+const String alt = "32"; //Enter your Altitude in meters without decimals
+
+long updateTime = 20 * 60000;  //Every 20 minutes
+unsigned long timeNow = 0;
 
 int rainCount = 0;
 int rainCountPerHour = 0;
-float rainHourly[24];  //store rainfall per hour 
-float rainTotal = 0;
-byte rainHour = 0; //keep track of wich hour we are up to
 
 /********Pin Defines***********/
 #define rainGauge 2 
@@ -27,35 +43,18 @@ byte rainHour = 0; //keep track of wich hour we are up to
 #define dhtPin 13          
 #define pwr 15      //9
 //4 & 5 = I2C   bmp180
-//#define batLevel   ADC
 
 
 /********Instances***********/
 Adafruit_BMP085 bmp;
-DHT dht22(dhtPin, DHT22, 20);  //3 for 8mHz, 6 for 16mHz, 30 for ESP 80Mhz
+DHT dht22(dhtPin, DHT22, 20);  //3 for 8mHz, 6 for 16mHz, 16-30 for ESP 80Mhz
 
 
-char headerHTML[] = "<!DOCTYPE html><html><head><title>Secluded.io Weather</title></head><body><h1>Real Time weather from Right Here!</h1>";
+String cmd = "";
+String packet = "";
 
-char tailHTML[] = "<p>Provided by Gold Coast Techspace</p></body></html>";
-char responseHTML[350] = "";
-char barString[30] = "";
-char humString[30] = "";
-char tempString[30] = "";
-char rainHrString[30] = "";
-char testString[18] = "";
-char rain24HrString[35] = "";
-
-
-//Function Prototypes
-void bmpInit();
-void dhtInit();
-void rainGageClick();
-void buildResponce();
-void gatherData();
-void calcUnitRain();
-
-void setup() {
+void setup() 
+{
   rainCountPerHour = 3600000 / updateTime; //Set how many time we check the rain gauge per hour
   pinMode(pwr, OUTPUT);
   digitalWrite(pwr, HIGH);
@@ -68,94 +67,100 @@ void setup() {
   
   timeNow = millis();
   
+  WiFi.begin(SSID, password);
   
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP("Secluded.io Weather");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  // if DNSServer is started with "*" for domain name, it will reply with
-  // provided IP to all DNS request
-  dnsServer.start(DNS_PORT, "*", apIP);
 
-  // replay to all requests with same HTML
-  buildResponce();
-  webServer.onNotFound([]() {
-    webServer.send(200, "text/html", responseHTML);
-  });
-  webServer.begin();
 }
 
-void loop() {
+void loop() 
+{
+
+  if (millis()-timeNow > updateTime){
+    Serial.print("connecting to ");
+    Serial.println(server);
   
-  if(millis()-timeNow > updateTime){
-    buildResponce();
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(server, httpPort)) {
+      Serial.println("connection failed");
+      return;
+    }
+  
+    buildPacket();   
+    buildCmd();
+    Serial.println(cmd);
+  
+    client.println(cmd);
+    delay(500);
+    
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    client.stop(); //Stopping client 
+  
     timeNow = millis();
   }
-  dnsServer.processNextRequest();
-  webServer.handleClient();
+}
+
+
+void buildCmd()
+{
+    //Build up POST request
+  cmd = "POST /data/post HTTP/1.1\n";
+  cmd += "Host: ";
+  cmd += server;
+  cmd += "\n";
+  cmd += "Content-Type: application/x-www-form-urlencoded\n";
+  cmd += "Authorization: Basic ";
+  cmd += CredBase64;
+  cmd += "\n";
+  cmd += "Content-Length: ";
+  cmd += packet.length();
+  cmd += "\n";
+  cmd += "Connection: close\n\n";
+  cmd += packet;
+  cmd += "\r\n\r\n";
+}
+
+
+void buildPacket()
+{  
+  double p = bmp.readPressure()/100.00;
+  double h = dht22.readHumidity();
+  double t = getTemp();
+  packet = "temp=";
+  packet += t, 2;
+  packet += "&humidity=";
+  packet += h;
+  packet += "&pressure=";
+  packet += p, 2;
+
+  //Only send rain data once per hour
+  if (rainCount == rainCountPerHour){
+    packet += "&rain_1hr=";
+    packet += calcUnitRain(), 2;
+  } else {
+    rainCount++;
+  }
   
-}
-
-void addStr(char *data){
-  Serial.println(data);
-  int len = strlen(responseHTML);
-  strcpy(responseHTML+len, data);
-}
-
-void buildResponce() {
-  calcUnitRain();
-  gatherData();
-  sprintf(responseHTML, "%s%s%s%s%s%s%s", headerHTML, tempString, barString, humString, rainHrString, rain24HrString, tailHTML);
-  Serial.println(responseHTML);
-}
-
-void gatherData(){
-  float bar = bmp.readPressure();
-  //Serial.println(bar);
-  //Serial.println(bmp.readPressure());
-  char barBuffer[7];
-  dtostrf(bar,6,0,barBuffer);
-  sprintf(barString, "%s%s%s", "<p>Barometric = ", barBuffer, " Pa</p>");
-  Serial.print(barString);
-  Serial.print(" :"); Serial.println(strlen(barString));
-
-  float temp = (bmp.readTemperature() + dht22.readTemperature()) / 2.0;
-  //Serial.println(temp);
-  char tempBuffer[5];
-  dtostrf(temp,2,2,tempBuffer);
-  sprintf(tempString, "%s%s%s", "<p>Temperature = ", tempBuffer, "*C</p>");
-  Serial.print(tempString);
-  Serial.print(" :"); Serial.println(strlen(tempString));
-
-  float hum = dht22.readHumidity();
-  //Serial.println(hum);
-  char humBuffer[5];
-  dtostrf(hum,2,2,humBuffer);
-  sprintf(humString, "%s%s%s", "<p>Humidity = ", humBuffer, "%</p>");
-  Serial.print(humString);
-  Serial.print(" :"); Serial.println(strlen(humString));
-
-  float rain = rainTotal;
-  char rainBuffer[7];
-  dtostrf(rain,3,2,rainBuffer);
-  sprintf(rainHrString, "%s%s%s", "<p>Hrly Rainfall = ", rainBuffer, "mm</p>");
-  Serial.print(rainHrString);
-  Serial.print(" :"); Serial.println(strlen(rainHrString));
-
-  float rain24 = 0.0;
-  for(byte i = 0; i < 24; i++) {
-    if(i == rainHour) {
-      rain24 += rainTotal;
-    } else {
-      rain24 += rainHourly[i];
-    }
-  } 
-  char rain24Buffer[7] = "24.67";
-  dtostrf(rain24,3,2,rain24Buffer);
-  sprintf(rain24HrString, "%s%s%s", "<p>24 Hrly Rainfall = ", rain24Buffer, "mm</p>"); 
-  //rain24HrString = "<p>24 Hrly Rainfall = 24.67mm</p>";
-  Serial.print(rain24HrString);
-  Serial.print(" :"); Serial.println(strlen(rain24HrString));
-  
+  packet += "&lat=";
+  packet += lat;
+  packet += "&long=";
+  packet += lng;
+  packet += "&alt=";
+  packet += alt;
+  packet += "&name=";
+  packet += StationName;
 }
 
--- sensors.ino
+++ sensors.ino
@@ -1,3 +1,34 @@
+//****************************RAIN*****************************
+
+#define RAIN_FACTOR 0.2794
+
+volatile float rainTipper=0.0;
+volatile unsigned long rainLastTip=0;
+ 
+double calcUnitRain(){ 
+  
+  double unitRain = rainTipper * RAIN_FACTOR;
+  
+
+  rainCount = 0;
+  rainTipper=0.0; 
+  
+  Serial.print("Rain Fall :");
+  Serial.print(unitRain);
+  Serial.println("mm");
+  return unitRain;
+}
+
+ 
+void rainGageClick(){
+  if(millis()-rainLastTip>400) {  //Stops debounce of the reed switch
+    rainTipper += 1.0;
+    rainLastTip=millis();
+  }
+}
+
+//**********************BMP*****************************************
+
 void bmpInit()
 {
   while(!bmp.begin()) {
@@ -7,69 +38,7 @@ void bmpInit()
   }
 }
 
-float getBarometric(){ //float *temp){
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-    
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
 
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
-    
-    Serial.println();
-    delay(500);
-}
-
-/*
-void get_bmp()
-{
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-    
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
-
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
-    
-    Serial.println();
-    delay(500);
-} */
 
 //**********************DHT*****************************************
 
@@ -91,113 +60,15 @@ void dhtInit()
     Serial.println(h);
     Serial.println(temp);
     //Serial.println("Failed to read from DHT sensor!");
-    //return 0.0;
+    //return 0.0;    
   } while (isnan(h) || isnan(temp));
 }
 
-void gettemperature() {
-    // Reading temperature for humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-    float hum = dht22.readHumidity();          // Read humidity (percent)
-    float temp = dht22.readTemperature();     // Read temperature
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(hum) || isnan(temp)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
-    Serial.println(hum);
-    Serial.println(temp);
-  
-}
 
-/*
-float getHumidity(float *temp, float *heatIndex)
+double getTemp()
 {
-  float h, f;
-  //dhtInit();
-  
-  // Check if any reads failed and exit early (to try again).
-  do {
-    Serial.println("Reading DHT sensor!");
-    //dht22.begin();
-    delay(100);
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    h = dht22.readHumidity();
-    // Read temperature as Celsius
-    *temp = dht22.readTemperature();
-    // Read temperature as Fahrenheit
-    f = dht22.readTemperature(true);    
-    //Serial.println("Failed to read from DHT sensor!");
-    //return 0.0;
-  } while (isnan(h) || isnan(*temp) || isnan(f));
-
-  // Compute heat index
-  // Must send in temp in Fahrenheit!
-  float hi = dht22.computeHeatIndex(f, h);
-
-    //
-  //dataToSend = buildString(h,5,"2=");
-
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  //espSerial.print(h);
-  //espSerial.print(',');
-  Serial.print(" %\t");
-//  Serial.print("Temperature: "); 
-//  Serial.print(t);
-//  Serial.print(" *C ");
-//  Serial.print(f);
-//  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  *heatIndex = ((hi-32)*5)/9;
-  Serial.print(*heatIndex);
-  //espSerial.print(heatIndex);
-  //espSerial.print(',');
-  Serial.println(" *C");
- 
-  
-  //Add Humidity to String
-  //dataToSend += "Humidity," + String(h) + "\r\n";  
-  
-  return h;
-} */
-
-//****************************RAIN*****************************
-
-#define RAIN_FACTOR 0.2794
-
-volatile float rainTipper=0.0;
-volatile unsigned long rainLastTip=0;
- 
-void calcUnitRain(){ 
-  
-  float unitRain = rainTipper * RAIN_FACTOR;
-  
-  if(rainCount < rainCountPerHour) {  //360 samples per hour
-    rainTotal += unitRain;  //add them all together
-    rainCount++;
-  } else {  //If we are over 1 hour
-    rainHourly[rainHour] = rainTotal;  //Store the total for that hour
-    rainTotal = 0.0;           //reset total
-    rainHour++;                //move to next hour
-    rainHourly[rainHour] = 0.0; //Clear previous data
-    if(rainHour == 24) {       //start again
-      rainHour = 0; 
-    }
-    rainCount = 0;        
-  }
-  
-  rainTipper=0.0;  
-  
-  Serial.print("Rain Fall :");
-  Serial.print(unitRain);
-  Serial.println("mm");
+  double t1 = bmp.readTemperature();
+  double t2 = dht22.readTemperature();
+  return t1 + t2 / 2;
 }
- 
-void rainGageClick(){
-  if(millis()-rainLastTip>400) {  //Stops debounce of the reed switch
-    rainTipper += 1.0;
-    rainLastTip=millis();
-  }
-}
+
